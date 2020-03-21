7575ef0005f3575a923ad787e81c06d027066e7c
swethabodepu <62110724+swethabodepu@users.noreply.github.com>
2020-03-21 19:22:09 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,14 @@
+class Pattern1
+{
+    public static void main(String a[])
+    {
+       for(int i=1;i<=5;i++)
+        {
+           for(int j=1;j<=5;j++)
+              {
+                System.out.printf("*");
+              }
+            System.out.println("\n");
+        }
+    }
+}
